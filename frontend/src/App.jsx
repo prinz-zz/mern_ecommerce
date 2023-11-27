0148418ea3a4889540fcdb6a5a74d21ffa0e3e95
Prinz Adamz <prinzadamz@gmail.com>
2023-11-27 17:22:06 +0530

    Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-
-import './App.css'
+import "./App.css";
+import Navigation from "./customer/components/Navigation";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+  return <>
+  <Navigation/>
+  </>;
 }
 
-export default App
+export default App;
